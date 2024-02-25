97955e43f4915a7f5490634136eff86505baa486
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2024-02-25 16:10:33 +0000

    retrieve number of each object

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,22 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """Returns the status of api"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats')
+def get_stats():
+    """Retrieves the number of each objects by type"""
+    classes = ['Amenity', 'City', 'Place', 'Review', 'State', 'User']
+    names = ['amenities', 'cities', 'places', 'reviews', 'states', 'users']
+    stats = {}
+
+    for idx, cls in enumerate(classes):
+        stats[names[idx]] = storage.count(cls)
+    return jsonify(stats)
