9611ef9e4da50e5fa70ff996406b086ffe90d9cc
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2024-02-26 07:59:45 +0000

    Create view for amenity

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -51,7 +51,7 @@ def delete_city(city_id):
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'],
                  strict_slashes=False)
-def post_city(state_id):
+def create_city(state_id):
     """Creates a City"""
     state = storage.get(State, state_id)
     if not state:
@@ -69,10 +69,8 @@ def post_city(state_id):
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
-def put_city(city_id):
-    """
-    Updates a City
-    """
+def update_city(city_id):
+    """Updates a City"""
     city = storage.get(City, city_id)
     if not city:
         abort(404)
